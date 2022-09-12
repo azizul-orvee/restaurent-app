72bbe5a6e6ec0f522067310123508c253e78d9ac
Azizul Hakim <mahadi@Mahadis-MacBook-Pro.local>
2022-09-13 01:30:04 +0600

    2nd main

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         This is the main, ahead of dev 1.
+         This is the main, ahead of dev 2.
         </p>
         <a
           className="App-link"
