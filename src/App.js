99b8575858470b340d20a1d0b29984688edd4602
Azizul Hakim <mahadi@Mahadis-MacBook-Pro.local>
2022-09-13 01:17:31 +0600

    fist rebase

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         This is the dev branch now mergin with main branch
+         This is the rebase.
         </p>
         <a
           className="App-link"
